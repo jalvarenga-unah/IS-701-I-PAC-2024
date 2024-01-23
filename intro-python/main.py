4456d2e4c86f0ecb1782b79f3aebafcbfe6ff288
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-01-23 16:43:00 -0600

    introducción a python, uso de variables

---
+++ intro-python/main.py
@@ -0,0 +1,14 @@
+print("Hola mundo")
+# print("Mi nombre es Juan Alvarenga")
+
+#comentario de una linea
+nombre = "Juan Alvarenga" # String
+edad = 29 # int
+sueldo = 1.02 # float
+casado = True # boolean
+
+# listas, diccionarios, tuplas, set
+
+# print(nombre + " tengo " + str(edad))
+print(type(edad)) # para saber el tipo de variable
+print(f"{nombre} tengo {edad} años") # otra forma de concatenar
